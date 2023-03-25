c21a2455192dd8d8a82e105d3ecbe43281c1d9ef
Samay Kumar <97271429+samay15jan@users.noreply.github.com>
2023-03-25 20:51:24 +0530

    Add files via upload

---
+++ arch-install.sh
@@ -0,0 +1,114 @@
+#!/bin/bash
+clear
+echo "
+               _ _ _ ____ _    ____ ____ _  _ ____    ___ ____    ____ ____ ____ _  _    _ _  _ ____ ___ ____ _    _    
+               | | | |___ |    |    |  | |\/| |___     |  |  |    |__| |__/ |    |__|    | |\ | [__   |  |__| |    |    
+               |_|_| |___ |___ |___ |__| |  | |___     |  |__|    |  | |  \ |___ |  |    | | \| ___]  |  |  | |___ |___ 
+
+"
+
+read -p "Please enter hostname: " hostname
+read -p "Please enter username: " username
+read -p "Please enter user password: " user_password
+read -p "Please enter root password: " root_password
+clear
+
+# Step 1
+loadkeys us
+pacman --noconfirm -Sy archlinux-keyring
+timedatectl set-ntp true
+wipefs -a /dev/sda1
+wipefs -a /dev/sda2
+wipefs -a /dev/sda
+sfdisk /dev/sda << EOF
+,500m
+;
+EOF
+mkfs.fat -F 32 /dev/sda1
+mkfs.ext4 /dev/sda2
+mount /dev/sda2 /mnt
+mkdir -p /mnt/boot/efi
+mount /dev/sda1 /mnt/boot/efi
+pacstrap /mnt base linux linux-firmware efibootmgr grub networkmanager sed nano sudo
+genfstab -U /mnt >> /mnt/etc/fstab
+echo "echo $hostname > /etc/hostname" >> /mnt/temp.sh
+echo "useradd -m -G wheel -s /bin/bash $username" >> /mnt/temp.sh
+echo "echo \"$username ALL=(ALL:ALL) ALL\" >> /etc/sudoers" >> /mnt/temp.sh
+echo "echo $username:$user_password | chpasswd" >> /mnt/temp.sh
+echo "echo root:$root_password | chpasswd" >> /mnt/temp.sh
+chmod u+x /mnt/temp.sh 
+sed '1,/^# Step 2$/d' `basename $0` > /mnt/arch_install2.sh
+chmod +x /mnt/arch_install2.sh
+arch-chroot /mnt ./arch_install2.sh
+exit
+
+# Step 2
+ln -sf /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+hwclock --systohc
+sed -i "/en_IN.UTF-8/s/^#//g" /etc/locale.gen
+locale-gen
+echo "LANG=en_IN.UTF-8" >> /etc/locale.conf
+echo "KEYMAP=us" > /etc/locale.conf
+./temp.sh
+rm -r /temp.sh
+grub-install /dev/sda
+sed -i 's/quiet/pci=noaer/g' /etc/default/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+systemctl enable NetworkManager.service
+location= /home/arch_install3.sh
+sed '1,/^# Step 3$/d' arch_install2.sh > /home/arch_install3.sh
+chmod +x /home/arch_install3.sh
+chown -R $username:$username /home/$username
+rm /arch_install2.sh
+clear
+echo "INSTALLATION COMPLETED PLEASE RESTART"
+exit
+
+# Step 3
+
+## Installing packages
+sudo pacman -S --noconfirm ttf-dejavu pango i3 dmenu ffmpeg jq curl \
+        alacritty pavucontrol go xorg openssh imagemagick unzip \
+        light git nautilus firefox base-devel python python-pip \
+        arandr feh bluez bluez-utils gmtp pamixer acpi xorg-xinit \
+        mpv neofetch qbittorrent code sxiv nano kdeconnect lynx \
+        pulseaudio sysstat android-file-transfer mtpfs gvfs-mtp ttf-font-awesome
+sudo pacman -R i3lock
+pip3 install pywal
+
+## Install yay and few packages
+git clone https://aur.archlinux.org/yay-git.git
+sudo chown -R $USER:$USER /home/$USER
+cd /home/$USER/yay-git
+makepkg -si
+yay -S --noconfirm pfetch jmtpfs picom-jonaburg-git i3lock-color python-pywalfox
+
+## Configuring
+echo "exec i3 " >> /home/$USER.xinitrc
+sudo systemctl enable bluetooth.service
+sudo chmod +s /usr/bin/light
+git clone https://github.com/samay15jan/dotfiles
+git clone https://github.com/samay15jan/wallpaper
+sudo chown -R $USER:$USER /home/$USER
+sudo chmod -R 777 /home/$USER/dotfiles/bin
+sudo mv -r /home/$USER/dotfiles/bin /usr/local
+sudo chown -R $USER:$USER /usr/local/bin/
+sudo chmod -R 777 /home/$USER/dotfiles/config/i3/scripts
+sudo chmod -R 777 /home/$USER/dotfiles/screenlayout/layout.sh
+sudo rm -r /home/$USER/dotfiles/.git 
+sudo mv /home/$USER/dotfiles/* /home/$USER
+mv /home/$USER/wallpaper Wallpaper
+mv /home/$USER/bashrc .bashrc
+mv /home/$USER/bash_profile .bash_profile
+mv /home/$USER/config .config
+mv /home/$USER/screenlayout .screenlayout
+mkdir /home/$USER/.cache /home/$USER/.cache/wal/
+sudo pywalfox install
+sudo rm -r /home/$USER/yay-git /home/$USER/arch-install /home/$USER/dotfiles
+sudo chown -R $USER:$USER /home/$USER
+clear
+echo "FINISHED"
+echo "ADDITIONAL MANUAL CONFIG
+Go to this link and install the extension for firefox pywal theme 
+LINK: https://addons.mozilla.org/en-US/firefox/addon/pywalfox/
+"
